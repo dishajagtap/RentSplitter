a729de9788babb85e0c79f16bc65f3230d12a985
Disha Jagtap <dishajagtap@dishas-mbp.attlocal.net>
2017-09-11 02:34:53 -0700

    added all ambrose cards

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ function hideAllContent(){
         document.getElementById(id_list[i]+"Content").style.display = 'none';
     }
 }
+
+function calculateStudioRent(){
+    var p = parseInt(document.getElementById("numberOfPersons").value);
+    var r = parseInt(document.getElementById("rentAmount").value);
+    
+    document.getElementById("result").value = r/p;   
+}
+
+function calculuateOneRent(sqft,bed1,common){
+    
+}
+
+function studioModalFill(label){
+    var m = document.getElementById("studioModalTitle");
+    m.innerHTML = label;
+}
+
+function oneModalFill(label){
+    var m = document.getElementById("oneModalTitle");
+    m.innerHTML = label;
+}
+
+function twotwoModalFill(label){
+    var m = document.getElementById("twotwoModalTitle");
+    m.innerHTML = label;
+}
